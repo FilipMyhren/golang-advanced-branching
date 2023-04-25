1fc1d2b8ae9e9135dd70b74f5350d8354de70093
FilipMyhren <filip.myhren@ingka.ikea.com>
2023-04-25 13:10:18 +0200

    Done with module 3

--- vehicle.go
+++ vehicle.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"encoding/json"
 	"io/ioutil"
 	"log"
@@ -82,6 +83,18 @@ func main() {
 	// Generate ratings for the different vehicles
 	generateRating()
 	// Print ratings for the different vehicles
+	for _, veh := range inventory {
+		switch v := veh.(type) {
+		case car:
+			v.carDetails()
+		case bike:
+			v.bikeDetails()
+		case truck:
+			v.truckDetails()
+		default:
+			fmt.Printf("Are you sure this Vehicle Type exists")
+		}
+	}
 }
 
 func readJSONFile() Values {
@@ -107,7 +120,7 @@ func generateRating() {
 		var vehRating rating
 		for _, msg := range v.Feedback {
 			if text := strings.Split(msg, " "); len(text) >= 5 {
-				vehRating = 5.0
+				vehRating = initial
 				vehResult.feedbackTotal++
 				for _, word := range text {
 					switch s := strings.Trim(strings.ToLower(word), " ,.,!,?,\t,\n,\r"); s {
@@ -133,7 +146,38 @@ func generateRating() {
 		}
 		vehicleResult[v.Name] = vehResult
 	}
+	fmt.Println(vehicleResult)
 }
 
+func showRating(model string) {
+	ratingFound := false
+	for m, r := range vehicleResult {
+		if m == model {
+			fmt.Printf("Total Ratings:%v\tPositive:%v\tNegative:%v\tNeutral:%v", 
+				r.feedbackTotal, 
+				r.feedbackPositive, 
+				r.feedbackNegative, 
+				r.feedbackNeutral)
+			ratingFound = true
+		}
+	}
+	if !ratingFound {
+		fmt.Printf("No rating for this vehicle")
+	}
+}
 
+func (c *car) carDetails() {
+	fmt.Printf("\n%-5v: %-8v: %-12v ", "Car" , c.make, c.model)
+	showRating(c.model)
+}
+
+func (b *bike) bikeDetails() {
+	fmt.Printf("\n%-5v: %-8v: %-12v ", "Bike" , b.make, b.model)
+	showRating(b.model)
+}
+
+func (t *truck) truckDetails() {
+	fmt.Printf("\n%-5v: %-8v: %-12v ", "Bike" , t.make, t.model)
+	showRating(t.model)
+}
 
